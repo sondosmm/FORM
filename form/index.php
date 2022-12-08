595da424dbb31d939d5f259f17637afdafe2902c
sondosmm <113656834+sondosmm@users.noreply.github.com>
2022-12-08 16:17:36 +0200

    Update index.php

--- form/index.php
+++ form/index.php
@@ -8,25 +8,7 @@
 </head>
 <body>
     
-    <?php
-// define variables and set to empty values
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $website = test_input($_POST["website"]);
-  $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
+ 
     <form action="welcome.php" method="post">
         <input type="text" name="name" placeholder="User Name"> <br> <br>
        <input type="email" name="email" placeholder="Email"> <br><br>
